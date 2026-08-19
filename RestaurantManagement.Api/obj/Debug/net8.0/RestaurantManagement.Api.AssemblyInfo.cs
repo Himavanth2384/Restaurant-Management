fa97c6e1ad60407e2c9ee9e463ab0dc01cbceacf
Himavanth2384 <nadellahimavanth1510@gmail.com>
2026-08-20 01:04:17 +0530

    commit

--- RestaurantManagement.Api/obj/Debug/net8.0/RestaurantManagement.Api.AssemblyInfo.cs
+++ RestaurantManagement.Api/obj/Debug/net8.0/RestaurantManagement.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantManagement.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3789a34ab47119df5ad729adc1ac52b9aee0d9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantManagement.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantManagement.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
