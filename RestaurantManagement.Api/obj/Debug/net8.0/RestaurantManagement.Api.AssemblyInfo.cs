f6165fc29a179e66119ce8c9df94d78e0db2f628
Himavanth2384 <nadellahimavanth1510@gmail.com>
2026-09-20 14:19:19 +0530

    css edit

--- RestaurantManagement.Api/obj/Debug/net8.0/RestaurantManagement.Api.AssemblyInfo.cs
+++ RestaurantManagement.Api/obj/Debug/net8.0/RestaurantManagement.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantManagement.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a4f8b8aeaa18f2b38c919b1addf7750782f5b78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35d82f1ff494924163b96c83bd5f0565b3a7fc71")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantManagement.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantManagement.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
